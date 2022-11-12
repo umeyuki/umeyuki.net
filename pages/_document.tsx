4284f6ad42f062c56a698e43f5642233d12fbfea
umeyuki <umeyuki@home.localdomain>
2022-11-12 17:05:50 +0900

    Update body class

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,9 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
-      <Html lang="en">
+      <Html lang="ja">
         <Head />
-        <body className="bg-white dark:bg-black text-gray-900 dark:text-white">
+        <body className="container mx-auto bg-white dark:bg-black text-gray-900 dark:text-white">
           <Main />
           <NextScript />
         </body>
