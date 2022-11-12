e1da28166de9aff9503d719778d1d512623c015c
umeyuki <umeyuki@home.localdomain>
2022-11-12 15:34:41 +0900

    wip design

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { MetaProps } from '../types/layout';
 import Head from './Head';
 import Navigation from './Navigation';
@@ -15,18 +16,23 @@ const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
   return (
     <>
       <Head customMeta={customMeta} />
-
       <header>
-        <div className="flex justify-start items-center p-4">
-          <Image
-            className="rounded-2xl"
-            src="/profile.jpeg"
-            alt="Profile Logo"
-            width={64}
-            height={64}
-          />
-          <h1 className="text-lg px-4">明日には忘れているだろう</h1>
-        </div>
+        <Link href="/">
+          <a>
+            <div className="flex justify-start items-center p-4">
+              <Image
+                className="rounded-2xl"
+                src="/profile.jpeg"
+                alt="Profile Logo"
+                width={64}
+                height={64}
+              />
+              <h1 className="text-lg px-4 text-black hover:text-gray-600">
+                明日には忘れているだろう
+              </h1>
+            </div>
+          </a>
+        </Link>
         <div className="max-w-5xl px-8 mx-auto">
           <div className="flex items-center justify-between py-6">
             <Navigation />
